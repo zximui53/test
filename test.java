8b40dc62dc4b54eb8ca2f010231dc54e983bca6b
Otha Zulauf <132994719+zximui53@users.noreply.github.com>
2023-07-11 14:26:28 +0800

    this is test
    this is test

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.err.println("this is java test");
         System.err.println("this is java test2");
+        System.err.println("this is java test3");
     }
 }
